2373f3047e97152eda1a72008f4d35cee39bc694
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-27 09:59:42 +0300

    Audio Practice

---
+++ android/app/src/main/java/app/xylo/xylo/MainActivity.java
@@ -0,0 +1,6 @@
+package app.xylo.xylo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(Xylo());
+}
+
+class Xylo extends StatefulWidget {
+  const Xylo({Key? key}) : super(key: key);
+
+  @override
+  State<Xylo> createState() => _XyloState();
+}
+
+class _XyloState extends State<Xylo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
